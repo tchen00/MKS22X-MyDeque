78e02f1aa447d8db1c622086452e04086aba3814
Tammy <tchen00@stuy.edu>
2019-03-21 11:46:49 -0400

    add unchecked

--- MyDeque.java
+++ MyDeque.java
@@ -2,8 +2,9 @@ public class MyDeque<E>{
   private E[] data;
   private int size, start, end;
 
+  @SuppressWarnings("unchecked") 
   public MyDeque(){
-    
+    data = (E[])new Object[10];
   }
 
   public MyDeque(int initialCapacity){
