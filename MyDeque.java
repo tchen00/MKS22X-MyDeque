371b6d40f4a206c18e8802b810a2db92327efcfe
Tammy <tchen00@stuy.edu>
2019-03-22 11:38:29 -0400

    add comments for methods

--- MyDeque.java
+++ MyDeque.java
@@ -2,6 +2,7 @@ public class MyDeque<E>{
   private E[] data;
   private int size, start, end;
 
+  // constructer and going pass warnings (if not give size)
   @SuppressWarnings("unchecked")
   public MyDeque(){
     data = (E[])new Object[10];
@@ -10,6 +11,7 @@ public class MyDeque<E>{
     size = data.length;
   }
 
+  // constructer and going pass warnings (if given size)
   @SuppressWarnings("unchecked")
   public MyDeque(int initialCapacity){
     data = (E[])new Object[initialCapacity];
@@ -24,37 +26,43 @@ public class MyDeque<E>{
 
   public String toString(){
     String output = "{";
-    for (int i = 0; i < data.length; i++){
+    for (int i = start; i < data.length; i++){
       output += data[i] + " ";
     }
     return output + "}";
   }
 
   public void addFirst(E element){
+    // throwing exception if element is not null
     if (element == null) throw new NullPointException("exception from addFirst");
   }
 
   public void addLast(E element){
+    // throwing exception if element is not null
     if (element == null) throw new NullPointException("exception from addLast");
 
   }
 
   public E removeFirst(){
+    // throwing exception if data set is empty
     if (size() == 0) throw new NoSuchElementException("from removeFirst");
 
   }
 
   public E removeLast(){
+    // throwing exception if data set is empty
     if (size() == 0) throw new NoSuchElementException("from removeLast");
 
   }
 
   public E getFirst(){
+    // throwing exception if data set is empty
     if (size() == 0) throw new NoSuchElementException("from getFirst");
     return data[start];
   }
 
   public E getLast(){
+    // throwing exception if data set is empty
     if (size() == 0) throw new NoSuchElementException("from getLast");
     return data[end];
   }
