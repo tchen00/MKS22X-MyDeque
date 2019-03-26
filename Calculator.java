7c4590f84741c89eaf9db59e44452f90f359dfab
Tammy <tchen00@stuy.edu>
2019-03-26 11:42:56 -0400

    calc

---
+++ Calculator.java
@@ -0,0 +1,8 @@
+public class Calculator{
+    /*Evaluate a postfix expression stored in s.
+     *Assume valid postfix notation, separated by spaces.
+     */
+    public static double eval(String s){
+      
+    }
+}
