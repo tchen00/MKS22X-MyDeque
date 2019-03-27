a742438d830d5ceca17531debff278de5216d819
Tammy <tchen00@stuy.edu>
2019-03-27 10:35:38 -0400

    begin calc - add:

--- Calculator.java
+++ Calculator.java
@@ -3,6 +3,16 @@ public class Calculator{
      *Assume valid postfix notation, separated by spaces.
      */
     public static double eval(String s){
-      
+      MyDeque<Double> stack = new MyDeque<Double>();
+      String[] split = s.split(" ");
+
+      for (String value: split){
+        // addition
+        if (value.equals("+")){
+          // adding the last two 
+          stack.addLast(stack.removeLast() + stack.removeLast());
+        }
+
+
     }
 }
